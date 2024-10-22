cc4c2c6b4288ae06230f611c676e7454a2c74b08
raychelleKalekye <kalekye.raychelle@strathmore.edu>
2024-10-22 12:53:42 +0300

    Added image slider to midsection

--- src/components/MidSection.js
+++ src/components/MidSection.js
@@ -1,16 +1,14 @@
-import React from'react';
-import Pic from '../assets/pizzaLeft.jpg';
-import '../styles/MidSection.css'
+import React from 'react';
+import ImageSlider from './imageSlider';
+import '../styles/MidSection.css';
 const MidSection = () => {
-    return (
-      <div className='MidSection'>
-        <div>
-            <img src={Pic} alt='PizzaLogo'/>
-            <button className='Btn'>ORDER NOW!</button>
-        </div>
-      </div>
-    )
-  }
-  
-  export default MidSection;
-  
+  return (
+    <div className='MidSection'>
+      <ImageSlider/>
+      <button className='OrderBtn'>ORDER NOW!</button>
+    </div>
+  );
+};
+
+
+export default MidSection;
---
+++ src/components/imageSlider.js
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+import { Zoom } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import Pic from '../assets/pizzaLeft.jpg';
+import Pic2 from '../assets/midSectionpizz3.jpg';
+
+
+const slideImage = [
+  { src: Pic, alt: 'Pizza Variety 1' },
+  { src: Pic2, alt: 'Pizza Variety 2' }
+ 
+];
+
+const ImageSlider = () => {
+  const zoomRef = useRef();  
+
+  const goToPrev = () => {
+    if (zoomRef.current) {
+      zoomRef.current.goToPrev();
+    }
+  };
+
+  const goToNext = () => {
+    if (zoomRef.current) {
+      zoomRef.current.goToNext();
+    }
+  };
+
+  return (
+    <div className='slider-wrapper'>
+      <Zoom ref={zoomRef} scale={1.05} autoplay={true} duration={3000} infinite={true}>
+        {slideImage.map((image, index) => (
+          <div key={index} className='each-slide'>
+            <img src={image.src} alt={image.alt} className='slider-image' />
+           
+          </div>
+        ))}
+      </Zoom>
+
+    
+      <div className="prev" onClick={goToPrev}>&#10094;</div>
+      <div className="next" onClick={goToNext}>&#10095;</div>
+    </div>
+  );
+};
+
+export default ImageSlider;
